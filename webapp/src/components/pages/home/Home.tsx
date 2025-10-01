d8c79be90d53206e3043b3934c9ad71098b47c01
Maria Layanne <MariaLayanne23@users.noreply.github.com>
2025-10-01 15:05:59 -0300

    ajustes de boões

--- webapp/src/components/pages/home/Home.tsx
+++ webapp/src/components/pages/home/Home.tsx
@@ -1,6 +1,6 @@
-import './Home.css';
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import "./Home.css";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import ChatButton from "../chat/chat";
 
 export default function Home() {
@@ -13,8 +13,8 @@ export default function Home() {
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     // envia filtros para a página de pesquisa
-    navigate('/search', {
-      state: { servico, entrada, saida }
+    navigate("/search", {
+      state: { servico, entrada, saida },
     });
   }
 
@@ -24,40 +24,44 @@ export default function Home() {
         <div className="hero-geral">
           <div className="hero">
             <div className="hero-content">
-              <p className='topo-esquerda'>Sem tempo para o <br /> passeio hoje?</p>
-              <p className='baixo-direita'>A gente leva por você!</p>
+              <p className="topo-esquerda">
+                Sem tempo para o <br /> passeio hoje?
+              </p>
+              <p className="baixo-direita">A gente leva por você!</p>
             </div>
 
             <div className="busca">
-              <form className='formBusca' onSubmit={handleSubmit}>
+              <form className="formBusca" onSubmit={handleSubmit}>
                 <div className="opcoes">
                   <label htmlFor="servicoId">
-                    <p className='busca-title'>Serviço</p>
+                    <p className="busca-title-p">Serviço</p>
                     <select
                       name="servicoId"
                       id="servicoId"
                       value={servico}
                       onChange={(e) => setServico(e.target.value)}
                     >
-                      <option value="" disabled>Selecione uma opção</option>
+                      <option value="" disabled>
+                        Selecione uma opção
+                      </option>
                       <option value="Passeio">Passeios</option>
                       <option value="PetSitting">Pet sitter</option>
                     </select>
                   </label>
                   <label htmlFor="enderecoId">
-                    <p className='busca-title'>Endereço</p>
+                    <p className="busca-title-e">Endereço</p>
                     <input
                       type="text"
                       name="enderecoId"
                       id="enderecoId"
-                      placeholder='Endereço'
+                      placeholder="Endereço"
                     />
                   </label>
                 </div>
 
                 <div className="opcoes-data">
                   <label htmlFor="entradaId">
-                    <p className='busca-title'>Entrada</p>
+                    <p className="busca-title">Entrada</p>
                     <input
                       type="date"
                       name="entradaName"
@@ -68,7 +72,7 @@ export default function Home() {
                     />
                   </label>
                   <label htmlFor="saidaId">
-                    <p className='busca-title'>Saída</p>
+                    <p className="busca-title">Saída</p>
                     <input
                       type="date"
                       name="saidaName"
@@ -81,7 +85,9 @@ export default function Home() {
                   </label>
                 </div>
 
-                <button type="submit" className='btn-buscar'>Buscar</button>
+                <button type="submit" className="btn-buscar">
+                  Buscar
+                </button>
               </form>
             </div>
           </div>
@@ -90,9 +96,11 @@ export default function Home() {
         <div className="info-atencao">
           <h2 className="info-title">Seu pet merece mais atenção e cuidado!</h2>
           <div className="info-background">
-            <p className='info-texto'>
-              No <span className='palavra-destaque'>Auwalk</span>, você encontra passeadores e pet sitters de confiança,
-              agenda serviços com <span className="palavra-destaque">poucos cliques</span> e acompanha tudo de forma segura e prática.
+            <p className="info-texto">
+              No <span className="palavra-destaque">Auwalk</span>, você encontra
+              passeadores e pet sitters de confiança, agenda serviços com{" "}
+              <span className="palavra-destaque">poucos cliques</span> e
+              acompanha tudo de forma segura e prática.
             </p>
           </div>
           <div className="imagem-gato"></div>
